a3f0d0a1eec724958df13c84699cfd4b27d4ea2d
R8ERPX <rachit.a@zoho.com>
2017-08-05 15:23:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-  res.send(createTemplate(articles[articleName]));
+  res.send(createTemplate(article[articleName]));
 });
 
 
