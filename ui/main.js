d566d48185402bb31854a69cb80d647bd3de7796
R8ERPX <rachit.a@zoho.com>
2017-08-05 17:15:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var img = document.getElementById('madi');
+var img = document.getElementById("ico");
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
