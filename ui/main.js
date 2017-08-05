b263c34e988f00a42c1a5f6b5171fd7c69eabc48
R8ERPX <rachit.a@zoho.com>
2017-08-05 17:08:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
 img.onClick = function() {
+    var interval = setInterval(moveleft, 100);
     img.style.marginLeft = '100px';
 }
