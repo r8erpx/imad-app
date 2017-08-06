cc9c728baa873e3480c978c9a630d4cbe10689f6
Rachit Arora <rachit.a@zoho.com>
2017-08-06 20:12:02 +0530

    Added To Do

--- server.js
+++ server.js
@@ -93,6 +93,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names = [];
+app.get('/submit-name/', function (req, res) {
+    var name = req.query.name; 
+    names.push(name);
+
+    res.send(JSON.stringify(names));
+
+});
+
+
 app.get('/counter', function(req,res) {
     counter = counter + 1;
     res.send(counter.toString());
